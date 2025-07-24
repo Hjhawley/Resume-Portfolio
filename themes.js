b3538c0c4a9b8d5cdeecee7bb6d4ffcc227461d9
Hayden <hjhawley7@gmail.com>
2025-07-23 18:23:30 -0600

    resume 2.0

--- themes.js
+++ themes.js
@@ -1,16 +1,17 @@
-const toggleButton = document.getElementById('theme-toggle');
-const storedTheme = localStorage.getItem('theme');
+const toggleButton = document.getElementById("theme-toggle");
+const storedTheme = localStorage.getItem("theme");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-const currentTheme = storedTheme || (prefersDarkScheme.matches ? "dark-mode" : "light-mode");
+const currentTheme =
+  storedTheme || (prefersDarkScheme.matches ? "dark-mode" : "light-mode");
 
 if (currentTheme === "dark-mode") {
-    document.body.classList.add("dark-mode");
+  document.body.classList.add("dark-mode");
 } else {
-    document.body.classList.remove("dark-mode");
+  document.body.classList.remove("dark-mode");
 }
 
-toggleButton.addEventListener('click', () => {
-    const isDark = document.body.classList.toggle("dark-mode");
-    localStorage.setItem("theme", isDark ? "dark-mode" : "light-mode");
+toggleButton.addEventListener("click", () => {
+  const isDark = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark-mode" : "light-mode");
 });
